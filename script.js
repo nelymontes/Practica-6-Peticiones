d43f0a1a7afc2d32300c0f547962034634da2a96
Nely Montes <njmontesdias@gmail.com>
2024-05-27 12:37:00 -0500

    Ejercicios de Peticiones con Api

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 console.log('peticiones')
 
+//callback - esto significa pasar funciones como parámetros de otras funciones
+const obtener = () =>{
+    console.log('obtener')
+}
+obtener()
+
+const buttonObtener = document.getElementById('button-obtener')
+//Agregar evento al botón
+//buttonObtener.addEventListener("click", obtener)
+
+// agregar eventos al botón se puede realizar con una función anónima
+buttonObtener.addEventListener("click", async () =>{
+console.log('obtener')
+const pokemonData = await obtenerPokemon()
+
+const pokemonArticle = document.getElementById("Pokemon")
+//crea un elemento html
+const h3 = document.createElement('h3')
+
+//le asina un texto al elemento
+
+h3.innerText = pokemonData.name
+//agregar un elemento como hijo
+pokemonArticle.appendChild(h3)
+
+//crear elemento img
+
+const img = document.createElement('img')
+img.src = pokemonData.sprites.front_default
+pokemonArticle.appendChild(img)
+
+})
+
 const obtenerPokemon = async () =>{
-    const respuesta  = await fetch ("https://pokeapi.co/api/v2/pokemon/pikachu")
+    let nombrePokemon = 'Pikachu'
+    const respuesta  = await fetch (`https://pokeapi.co/api/v2/pokemon/pikachu`)
     const data = await respuesta.json()
     console.log(data)
+    return data
 }
-obtenerPokemon()
